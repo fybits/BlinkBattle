cd154e7e550acd29795500322ce206ab2c6d5b2d
Whisp <incwhisper@gmail.com>
2019-04-27 01:02:29 +0300

    arena initial

---
+++ Assets/Arena/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int id;
+
+    
+    Vector2 moveDir;
+    Vector2 viewDir;
+
+    Vector2 vel;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sprite playerSkin = Resources.Load<Sprite>("Arena/Sprites/Player/player_" + id.ToString());
+        if (playerSkin)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = playerSkin;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        
+    }
+}
