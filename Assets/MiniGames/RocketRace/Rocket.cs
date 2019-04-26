7a293e98e41059af104ed608781364ed536a8e66
fybits <sergeig1313@gmail.com>
2019-04-27 01:00:58 +0300

    Add inputs

--- Assets/MiniGames/RocketRace/Rocket.cs
+++ Assets/MiniGames/RocketRace/Rocket.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     public int playerNum;
+    Vector2 vel;
+    public float maxSpeed = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,13 +16,13 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 vel = new Vector2(Input.GetAxisRaw("horizontalP" + playerNum),
-                                    Input.GetAxisRaw("actionP" + playerNum)*2) * 0.5f;
-        if (Mathf.Abs(vel.x) < 0.005)
-            vel.x = 0;
-        if (vel.y == 0)
-            vel.y = -0.3f;
+        Vector2 input = new Vector2(Input.GetAxisRaw("HorizontalP" + playerNum) * 2f,
+                                    Input.GetAxisRaw("ActionP" + playerNum) * 0.5f);
+        if (Mathf.Abs(input.x) < 0.005)
+            input.x = 0;
+        vel += (input + Vector2.down * 0.2f) * Time.deltaTime;
 
+        vel = Vector2.ClampMagnitude(vel, maxSpeed/5);
         Vector3 newPos = transform.position + new Vector3(vel.x, vel.y, 0);
         if (newPos.x < Camera.main.ViewportToWorldPoint(new Vector3(0.05f, 0, 0)).x)
             newPos.x = Camera.main.ViewportToWorldPoint(new Vector3(0.05f, 0, 0)).x;
@@ -28,7 +30,7 @@ public class Rocket : MonoBehaviour
         if (newPos.x > Camera.main.ViewportToWorldPoint(new Vector3(0.4f, 0, 0)).x)
             newPos.x = Camera.main.ViewportToWorldPoint(new Vector3(0.4f, 0, 0)).x;
         transform.position = newPos;
-        vel.x *= 0.85f;
+        vel.x *= 0.5f;
         Debug.Log(vel);
     }
 }
