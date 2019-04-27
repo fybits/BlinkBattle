5c6584d669af882944c79f5a23b9c6b394721e91
fybits <sergeig1313@gmail.com>
2019-04-27 23:49:26 +0300

    new GameManager and MiniGames UIManager

---
+++ Assets/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    enum GameState { MainMenu, MiniGames, Arena }
+    GameState state = GameState.MainMenu;
+    bool stateChanged = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (stateChanged) {
+            switch (state) {
+                case GameState.MainMenu:
+                    SceneManager.LoadScene("MainMenu");
+                    break;
+            }
+
+        }
+    }
+}
