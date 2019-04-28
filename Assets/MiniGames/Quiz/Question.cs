14c2e1bab8bb2b79dd7271d05deac448f35b98ce
fybits <sergeig1313@gmail.com>
2019-04-28 13:04:13 +0300

    Quiz Manager

---
+++ Assets/MiniGames/Quiz/Question.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Question
+{
+    public string question;
+    public string[] answers;
+    public int correctAnswerId;
+
+    public Question(string question, string[] answers, int correctId) {
+        this.question = question.ToUpper();
+        this.answers = new string[4];
+        for (int i = 0; i < 4; i ++) {
+            this.answers[i] = answers[i].ToUpper();
+        }
+        this.correctAnswerId = correctId;
+    }
+}
---
+++ Assets/MiniGames/Quiz/QuizManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class QuizManager : MonoBehaviour {
+    public Question[] quests;
+    Stack<Question> questions = new Stack<Question>();
+
+    public TextMeshProUGUI questionText;
+    public GameObject answers;
+
+    bool alloyInput = false;
+
+    int player1Choice, player2Choice;
+
+    private void Start() {
+        for (int i = 0; i < quests.Length; i++) {
+            questions.Push(quests[i]);
+        }
+    }
+
+
+    IEnumerator Loop() {
+        Question current = questions.Pop();
+        questionText.text = current.question;
+        TextMeshProUGUI[] variants = answers.GetComponentsInChildren<TextMeshProUGUI>();
+        for (int i = 0; i < 4; i++) {
+
+            yield return new WaitForSeconds(0.1f);
+        }
+        alloyInput = true;
+    }
+}
