98aea364d52c938bf4c6a157b8484aabb1a7bd77
WhisperAbuse <incwhisper@gmail.com>
2019-04-28 12:17:15 +0300

    weapon market

--- Assets/Arena/Scripts/Shop/WeaponItem.cs
+++ Assets/Arena/Scripts/Shop/WeaponItem.cs
@@ -6,30 +6,59 @@ using UnityEngine.UI;
 
 public class WeaponItem : MonoBehaviour
 {
-    public Button button;
-    public TextMeshProUGUI text;
-
     public int playerId;
-    public int weaponId;
-    public int price;
-    public Sprite sprite;
+    public Image image;
 
     public ArenaManager arenaManager;
-    
 
+    int weaponId;
+    int price;
+    public bool isBought = false;
     // Start is called before the first frame update
     void Start()
     {
         weaponId = Random.Range(1, arenaManager.weapons.Length + 1);
-        price = arenaManager.weapons[weaponId].GetComponent<Weapon>().price;
-        sprite = arenaManager.weapons[weaponId].GetComponent<SpriteRenderer>().sprite;
+        price = arenaManager.weapons[weaponId - 1].GetComponent<Weapon>().price;
+
+        Sprite sprite = arenaManager.weapons[weaponId - 1].GetComponent<SpriteRenderer>().sprite;
+        TextMeshProUGUI text = GetComponentInChildren<TextMeshProUGUI>();
         text.text = price.ToString();
-        button.gameObject.GetComponent<Image>().sprite = sprite;
+        image.sprite = sprite;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    public void BuyWeapon()
+    {
+        if (!isBought)
+        {
+            if (playerId == 1 && arenaManager.pl1weaponId == 0)
+            {
+                if (arenaManager.pl1Score >= price)
+                {
+                    arenaManager.pl1weaponId = weaponId;
+                    arenaManager.pl1Score -= price;
+                    arenaManager.pl1ScoreText.text = arenaManager.pl1Score.ToString();
+                    GetComponent<Image>().color = new Color(0xFF / 255f, 0xD6 / 255f, 0x4C / 255f);
+                    isBought = true;
+                }
+            }
+            else if (playerId == 2)
+            {
+                if (arenaManager.pl2Score >= price && arenaManager.pl2weaponId == 0)
+                {
+                    arenaManager.pl2weaponId = weaponId;
+                    arenaManager.pl2Score -= price;
+                    arenaManager.pl2ScoreText.text = arenaManager.pl2Score.ToString();
+                    GetComponent<Image>().color = new Color(0xFF/255f, 0xD6/255f, 0x4C/255f);
+                    isBought = true;
+                }
+            }
+        }
     }
 }
